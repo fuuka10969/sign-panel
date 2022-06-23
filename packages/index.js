bfad8d2b081b65b92858f591bf986d604bef16a1
dongfeiying <dongfeiying@rincers.com>
2022-06-23 11:22:19 +0800

    feat: file clean

--- packages/index.js
+++
@@ -1,7 +0,0 @@
-// 导入单个组件
-import SignPanel from './SignPanel';
-SignPanel.install = function (Vue) {
-    Vue.component(SignPanel.name, SignPanel);
-}
-// 导出的对象必须具备一个 install 方法
-export default SignPanel;
